ed969a27f482f14c382656e224bf8ece9b06065c
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-05-16 04:44:35 +0500

    libra: fix build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/viper_libra.mk
-
-COMMON_LUNCH_CHOICES := \
-    viper_libra-eng \
-    viper_libra-user \
-    viper_libra-userdebug
+    $(LOCAL_DIR)/viper.mk
