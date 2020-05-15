31788b147a11b6025fbee3ed00e9e69257d43bcb
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-05-15 20:35:34 +0500

    libra: evo-fy

--- aosp_libra.mk
+++ aosp_libra.mk
@@ -21,18 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
-# Inherit some common DerpFest stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common Evo stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-DERP_BUILD_ZIP_TYPE := VANILLA
-DERP_BUILDTYPE := Official
+TARGET_BOOT_ANIMATION_RES_EVO := true
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/libra/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := libra
-PRODUCT_NAME := derp_libra
+PRODUCT_NAME := aosp_libra
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi-4c
 PRODUCT_MANUFACTURER := Xiaomi
