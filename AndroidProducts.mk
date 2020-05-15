e389171766392b874c8c2c6090af25ee6bf79c65
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-05-15 22:50:18 +0500

    libra: fix derp

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_libra.mk
+    $(LOCAL_DIR)/aosp_libra.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_libra-eng \
-    derp_libra-user \
-    derp_libra-userdebug
+    aosp_libra-eng \
+    aosp_libra-user \
+    aosp_libra-userdebug
