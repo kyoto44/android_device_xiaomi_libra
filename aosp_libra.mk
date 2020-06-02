f2f6372368f7e60996c0ffbbcb96dd01679f8719
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-06-02 22:53:59 +0500

    Revert "libra: Enable go mode"
    This reverts commit 025dbd058391d4a2660ad8ce145f33e27707c9a6.

--- aosp_libra.mk
+++ aosp_libra.mk
@@ -21,9 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
-# Enable go mode
-$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
-
 # Inherit some common Evo stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
