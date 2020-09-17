7b21b1431be6aadf99e6e928e15c11e975d7107c
Dmitry <kyoto1337@protonmail.com>
2020-09-17 20:08:00 +0500

    libra: Nuke libinit
    Unsupported for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,10 +118,6 @@ TARGET_USES_OVERLAY := true
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_libra
-TARGET_RECOVERY_DEVICE_MODULES := libinit_libra
-
 # IPA
 USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR := true
 
--- system_prop.mk
+++ system_prop.mk
@@ -182,3 +182,11 @@ persist.service.adb.enable=1 \
 persist.service.debuggable=1 \
 persist.sys.usb.config=mtp,adb \
 ro.adb.secure=0
+
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=2m \
+dalvik.vm.heapmaxfree=8m
