355b1481b1aed0a50b7b2783896e82b7dfd4ed63
Dmitry Gomzyakov <nicerok11@gmail.com>
2020-09-23 02:19:03 +0500

    libra: improve lights HAL

--- light/service.cpp
+++ light/service.cpp
@@ -1,11 +1,12 @@
 /*
- * Copyright 2019 The LineageOS Project
+ * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,36 +17,34 @@
 
 #define LOG_TAG "android.hardware.light@2.0-service.xiaomi_msm8994"
 
-#include "Light.h"
+#include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
+
+#include "Light.h"
 
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
-using android::hardware::light::V2_0::ILight;
 using android::hardware::light::V2_0::implementation::Light;
 
 using android::OK;
-using android::sp;
 using android::status_t;
 
-int main()
-{
-    android::sp<ILight> service = new Light();
+int main() {
+    android::sp<Light> service = new Light();
 
     configureRpcThreadpool(1, true);
 
     status_t status = service->registerAsService();
-    if (status != OK)
-    {
-        ALOGE("Cannot register Light HAL service.");
+    if (status != OK) {
+        LOG(ERROR) << "Cannot register Light HAL service.";
         return 1;
     }
 
-    ALOGI("Light HAL service ready.");
+    LOG(DEBUG) << "Light HAL service ready.";
 
     joinRpcThreadpool();
 
-    ALOGI("Light HAL service failed to join thread pool.");
+    LOG(ERROR) << "Light HAL service failed to join thread pool.";
     return 1;
 }
