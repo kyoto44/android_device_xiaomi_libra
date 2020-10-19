f18c4eef320bcaf64526b35dfa776c14b73e29b0
Dmitry <kyoto1337@protonmail.com>
2020-10-19 12:57:17 +0500

    libra: use zram writeback same as pixels

--- system_prop.mk
+++ system_prop.mk
@@ -183,3 +183,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Charger
 PRODUCT_PROPERTY_OVERRIDES += \
    ro.charger.enable_suspend=true
+
+   # ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
