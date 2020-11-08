09e7ef90b927fa022c8bb968f9894a739766f647
Dmitry <kyoto1337@protonmail.com>
2020-11-09 01:33:06 +0500

    libra: Build android.hardware.bluetooth.a2dp@1.0-impl
    * properly binderized a2dp

--- device.mk
+++ device.mk
@@ -47,6 +47,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.soundtrigger@2.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-impl \
     audio.a2dp.default \
     audio.primary.msm8992 \
     audio.r_submix.default \
