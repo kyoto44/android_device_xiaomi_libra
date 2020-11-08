f84f4d6a0f101d1eba2b0d49e42ca4324d33eb27
Dmitry <kyoto1337@protonmail.com>
2020-11-08 05:03:31 +0500

    libra: Switch to Audio HAL V6

--- device.mk
+++ device.mk
@@ -43,9 +43,18 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/vendor/etc/usb_audio_policy_configuration.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@6.0 \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.common@6.0 \
+    android.hardware.audio.common@6.0-util \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.audio.effect@6.0 \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service \
     audio.a2dp.default \
     audio.primary.msm8992 \
     audio.r_submix.default \
