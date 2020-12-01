f4237ac1a1ede90ede5e04c4aed2d9bb63a48d11
Dmitry <kyoto1337@protonmail.com>
2020-12-01 22:44:53 +0500

    libra: BoardConfig: Set TARGET_CPU_VARIANT as 'cortex-a53'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,9 +26,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_CORTEX_A53 := true
-TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a53
 
 
 # Assertions
